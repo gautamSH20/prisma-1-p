1b0a96975df157a0ac3673b25db8c7661b3a5235
gautamSH20 <gs841400@gmail.com>
2025-03-04 11:35:26 +0530

    db created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/user", (req, res) => {
+  const { name } = req.body;
+
+  res.send(name);
+});
+
+app.listen(3000, () => {
+  console.log("working");
+});
